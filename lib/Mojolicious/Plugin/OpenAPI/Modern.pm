03d7dbf63d1ec3052f1dec44b848e8bdda5fd929
Karen Etheridge <ether@cpan.org>
2024-12-20 12:55:22 -0800

    increment $VERSION after 0.013 release

--- lib/Mojolicious/Plugin/OpenAPI/Modern.pm
+++ lib/Mojolicious/Plugin/OpenAPI/Modern.pm
@@ -4,7 +4,7 @@ package Mojolicious::Plugin::OpenAPI::Modern;
 # ABSTRACT: Mojolicious plugin providing access to an OpenAPI document and parser
 # KEYWORDS: validation evaluation JSON Schema OpenAPI Swagger HTTP request response
 
-our $VERSION = '0.013';
+our $VERSION = '0.014';
 
 use 5.020;
 use if "$]" >= 5.022, experimental => 're_strict';
