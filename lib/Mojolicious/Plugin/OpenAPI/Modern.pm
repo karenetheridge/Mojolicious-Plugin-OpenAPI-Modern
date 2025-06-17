02db1996883f8e6a996e53ae3c2d09e1153b4613
Karen Etheridge <ether@cpan.org>
2025-06-17 14:31:14 -0700

    add some more pragmas

--- lib/Mojolicious/Plugin/OpenAPI/Modern.pm
+++ lib/Mojolicious/Plugin/OpenAPI/Modern.pm
@@ -7,10 +7,13 @@ package Mojolicious::Plugin::OpenAPI::Modern;
 our $VERSION = '0.017';
 
 use 5.020;
+use utf8;
 use if "$]" >= 5.022, experimental => 're_strict';
 no if "$]" >= 5.031009, feature => 'indirect';
 no if "$]" >= 5.033001, feature => 'multidimensional';
 no if "$]" >= 5.033006, feature => 'bareword_filehandles';
+no if "$]" >= 5.041009, feature => 'smartmatch';
+no feature 'switch';
 use Mojo::Base 'Mojolicious::Plugin', -signatures;
 use Feature::Compat::Try;
 use YAML::PP;
